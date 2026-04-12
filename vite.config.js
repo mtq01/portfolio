3c075282f0e4e7b3703f2e7ff087043c6425860f
MIKE <187060323+mtq01@users.noreply.github.com>
2026-04-11 21:07:05 -0700

    added htaccess and updated vite.config.js, added global app folder name to globals

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/emburr/',
+  build: {
+    outDir: 'emburr'
+  },
   plugins: [react()],
+  css: {
+    devSourcemap: true,
+  },
 })
